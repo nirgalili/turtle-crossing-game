ae67a0ec4781d6a010dfc3a3fe96cbc60c02dbce
Nir Galili <nirgalili1@gmail.com>
2021-08-24 16:26:39 +0300

    turtle move up

--- player.py
+++ player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        # self.hideturtle()
+        self.penup()
+        self.shape("turtle")
+        self.color("black")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        # self.showturtle()
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+
+
